84093f04a45bf603eeffcc3789d1379ce23e7435
Safiyat Reza <md.reza@snapdeal.com>
2016-06-09 15:05:17 +0530

    Added filters.

--- cicd/vm_info.py
+++ cicd/vm_info.py
@@ -45,3 +45,56 @@ def get_vm_list(output='file', **kwargs):
         print vm_list_json
 
     return vm_list_json
+
+
+def filter_vms(vm_list, **kwargs):
+    """
+    Filter the vm list for vms based on specified parameters.
+
+    :param list vm_list: A list of dictionaries containing information about
+                         the VMs.
+
+    kwargs:
+        :param string name:       The name of the VMs to be filtered.
+        :param string ip_addr:    The IP address of the VMs to be filtered.
+        :param string flavor_id:     The flavor ID of the VMs to be filtered.
+        :param string uuid:       The uuid of the VMs to be filtered.
+        :param string tenant_id:  The tenant ID of the VMs to be filtered.
+        :param string user_id:    The user ID of the VMs to be filtered.
+        :param string status:     Status of the VMs to be filtered.
+        :param string hypervisor: The hypervisor of the VMs to be filtered.
+    """
+    filter_list = ['name', 'ip_addr', 'flavor_id', 'uuid', 'tenant_id',
+                   'user_id', 'status', 'hypervisor']
+
+    # filters = {}
+    for (key, value) in kwargs.items():
+        if key not in filter_list:
+            raise(Exception('Invalid keyword argument: %s' % key))
+        # filters[key] = value
+
+    filtered = vm_list
+
+    if 'name' in kwargs:
+        filtered = filter(lambda d: d['name'] == kwargs['name'], filtered)
+    if 'ip_addr' in kwargs:
+        filtered = filter(lambda d: d['addresses'].values()[0][0]['addr'] ==
+                          kwargs['ip_addr'], filtered)
+    if 'flavor_id' in kwargs:
+        filtered = filter(lambda d: d['flavor']['id'] == kwargs['flavor_id'],
+                          filtered)
+    if 'uuid' in kwargs:
+        filtered = filter(lambda d: d['id'] == kwargs['uuid'], filtered)
+    if 'tenant_id' in kwargs:
+        filtered = filter(lambda d: d['tenant_id'] == kwargs['tenant_id'],
+                          filtered)
+    if 'user_id' in kwargs:
+        filtered = filter(lambda d: d['user_id'] == kwargs['user_id'],
+                          filtered)
+    if 'status' in kwargs:
+        filtered = filter(lambda d: d['status'] == kwargs['status'], filtered)
+    if 'hypervisor' in kwargs:
+        filtered = filter(lambda d: d['OS-EXT-SRV-ATTR:hypervisor_hostname'] ==
+                          kwargs['hypervisor'], filtered)
+
+    return filtered
