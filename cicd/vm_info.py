fcb4b2cf2e95c6ed0cab56432824edffc64891af
Safiyat Reza <md.reza@snapdeal.com>
2016-06-09 17:03:58 +0530

    Added print function.

--- cicd/vm_info.py
+++ cicd/vm_info.py
@@ -98,3 +98,43 @@ def filter_vms(vm_list, **kwargs):
                           kwargs['hypervisor'], filtered)
 
     return filtered
+
+
+def print_vm_info(vm_list):
+    if type(vm_list) is not list:
+        vm_list = [vm_list]
+    headers = ['ID', 'Name', 'Host', 'User', 'Tenant', 'Status']
+    column_width = [36, 4, 4, 4, 32, 13]
+    output_list = []
+    for vm in vm_list:
+        info = {'id': vm['id']}
+        info['name'] = vm['name']
+        info['hypervisor'] = vm['OS-EXT-SRV-ATTR:host']
+        info['user'] = vm['user_id']
+        info['tenant'] = vm['tenant_id']
+        info['status'] = vm['status']
+        output_list.append(info)
+
+        if column_width[1] < len(info['name']):
+            column_width[1] = len(info['name'])
+        if column_width[2] < len(info['hypervisor']):
+            column_width[2] = len(info['hypervisor'])
+        if column_width[3] < len(info['user']):
+            column_width[3] = len(info['user'])
+
+    header_line = '+%s+' % ('+'.join(['-' * (x + 2) for x in column_width]))
+    for i in range(len(headers)):
+        headers[i] = headers[i].center(column_width[i] + 2)
+
+    print header_line
+    print '|%s|' % ('|'.join(headers))
+    print header_line
+    for info in output_list:
+        print '| %s | %s | %s | %s | %s | %s |' % (
+            info['id'].ljust(column_width[0]),
+            info['name'].ljust(column_width[1]),
+            info['hypervisor'].ljust(column_width[2]),
+            info['user'].ljust(column_width[3]),
+            info['tenant'].ljust(column_width[4]),
+            info['status'].ljust(column_width[5]))
+    print header_line
